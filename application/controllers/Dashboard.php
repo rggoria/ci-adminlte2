0c543eef3c836239f8036e4db8eff3a891a4fd3a
Ram Emerson Goria <goria_reg@gmail.com>
2023-04-23 22:46:31 +0800

    Update Split of Components

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,6 +5,11 @@ class Dashboard extends CI_Controller
 {
     public function index()
     {
+        $this->load->view('dashboard/header');
+        $this->load->view('dashboard/navbar');
+        $this->load->view('dashboard/left-sidebar');
+        $this->load->view('dashboard/right-sidebar');
         $this->load->view('dashboard/index');
+        $this->load->view('dashboard/footer');
     }
 }
